e71a6cf9d3aed1c20dd7df2613f7b3b7021ff0c2
Colin Grasley <147641960+ColinGrasley@users.noreply.github.com>
2024-12-03 17:46:47 -0600

    added java files to main
    added the controllers for start and gameplay fxml files. as well as an updated version of the JavaFXTemplate class

--- Client/src/main/java/JavaFXTemplate.java
+++ Client/src/main/java/JavaFXTemplate.java
@@ -4,6 +4,7 @@ import javafx.animation.RotateTransition;
 import javafx.animation.SequentialTransition;
 import javafx.application.Application;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
@@ -16,47 +17,20 @@ import javafx.util.Duration;
 public class JavaFXTemplate extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		launch(args);
 	}
 
-	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		primaryStage.setTitle("Welcome to JavaFX");
-		
-		 Rectangle rect = new Rectangle (100, 40, 100, 100);
-	     rect.setArcHeight(50);
-	     rect.setArcWidth(50);
-	     rect.setFill(Color.VIOLET);
+		primaryStage.setTitle("Three Card Poker");
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/StartMenu.fxml"));
+		VBox root = loader.load();
 
-	     RotateTransition rt = new RotateTransition(Duration.millis(5000), rect);
-	     rt.setByAngle(270);
-	     rt.setCycleCount(4);
-	     rt.setAutoReverse(true);
-	     SequentialTransition seqTransition = new SequentialTransition (
-	         new PauseTransition(Duration.millis(500)),
-	         rt
-	     );
-	     seqTransition.play();
-	     
-	     FadeTransition ft = new FadeTransition(Duration.millis(5000), rect);
-	     ft.setFromValue(1.0);
-	     ft.setToValue(0.3);
-	     ft.setCycleCount(4);
-	     ft.setAutoReverse(true);
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("styles/startStyle.css").toExternalForm());
 
-	     ft.play();
-	     BorderPane root = new BorderPane();
-	     root.setCenter(rect);
-	     
-	     Scene scene = new Scene(root, 700,700);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		
-				
-		
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 }
---
+++ Client/src/main/java/StartMenuController.java
@@ -0,0 +1,47 @@
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class StartMenuController implements Initializable {
+    @FXML
+    Label reference;
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void handleSwitchToGamePlay() {
+        //TODO need to handle the server stuff with this as well
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/GamePlayGUI.fxml"));
+            Scene gameScene = new Scene(loader.load());
+            gameScene.getStylesheets().add(getClass().getResource("styles/style1.css").toExternalForm());
+            Stage stage = (Stage)(reference.getScene().getWindow());
+            stage.setScene(gameScene);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.console().printf("Error: %s%n", e.getMessage());
+        }
+
+    }
+
+    //Exits the game from the start button
+    @FXML
+    public void handleExitGame() {
+        System.exit(0);
+
+    }
+
+}

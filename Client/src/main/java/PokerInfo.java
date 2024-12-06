4b8c440bd51b16c440920a44b4815060cad01091
c-bejar <p.bejar.christopher@gmail.com>
2024-12-06 14:42:59 -0600

    made serializable and added  count for client

--- Client/src/main/java/PokerInfo.java
+++ Client/src/main/java/PokerInfo.java
@@ -1,5 +1,12 @@
-public class PokerInfo {
+import java.io.Serializable;
+
+public class PokerInfo implements Serializable{
+    public int count;
     public String command;
     public String playerBets;
     public boolean foldOrPlay;
+
+    PokerInfo(int count) {
+        this.count = count;
+    }
 }

f38bbd96b07881fefc94fd781a4fa8d34a8e13dd
c-bejar <p.bejar.christopher@gmail.com>
2024-12-06 16:42:51 -0600

    server additions

--- Server/src/main/java/ServerController.java
+++ Server/src/main/java/ServerController.java
@@ -1,21 +1,26 @@
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.application.Platform;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 
+
 public class ServerController {
+    @FXML Label clientsLabel;
     @FXML Button activateServer;
+    @FXML VBox clientJoinLog;
     Server serverConnection;
 
     public void activateServerButton(ActionEvent e) {
         activateServer.setDisable(true);
-        // e.getSource().setDisable(true);
         System.out.println("Pressed!");
 
         serverConnection = new Server(data -> {
             Platform.runLater(() -> {
-                //TODO do something with data
                 System.out.println("Server Received: "+data.toString());
+                clientJoinLog.getChildren().add(new Label(data.toString()));
+                clientsLabel.setText("Clients Connected: "+serverConnection.count);
             });
         });
     }

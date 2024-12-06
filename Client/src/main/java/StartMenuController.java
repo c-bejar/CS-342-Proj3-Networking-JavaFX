8e1961724a9ac57af147877ba8c440f0d5b5cc11
Colin Grasley <147641960+ColinGrasley@users.noreply.github.com>
2024-12-06 17:18:26 -0600

    checking changes

--- Client/src/main/java/StartMenuController.java
+++ Client/src/main/java/StartMenuController.java
@@ -1,4 +1,4 @@
-import javafx.application.Platform;
+import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -7,20 +7,26 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class StartMenuController implements Initializable{
-    @FXML Label reference;
-    Client clientConnection;
+public class StartMenuController implements Initializable {
+    @FXML
+    Label reference;
 
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
     public void handleSwitchToGamePlay() {
         //TODO need to handle the server stuff with this as well
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/GamePlayGUI.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/GamePlayGUI.fxml"));
             Scene gameScene = new Scene(loader.load());
-            gameScene.getStylesheets().add("/styles/style1.css");
+            gameScene.getStylesheets().add(getClass().getResource("styles/style1.css").toExternalForm());
             Stage stage = (Stage)(reference.getScene().getWindow());
             stage.setScene(gameScene);
 
@@ -32,19 +38,9 @@ public class StartMenuController implements Initializable{
     }
 
     //Exits the game from the start button
+    @FXML
     public void handleExitGame() {
         System.exit(0);
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        clientConnection = new Client(data->{
-            Platform.runLater(() -> {
-                System.out.println("Client Received: "+data.toString());
-            });
-        });
-
-        clientConnection.start();
 
     }
 

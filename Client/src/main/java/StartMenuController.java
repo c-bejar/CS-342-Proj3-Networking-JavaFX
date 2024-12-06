1947f785ddaf722472cb8c08084025bb02ba1bcc
c-bejar <p.bejar.christopher@gmail.com>
2024-12-06 14:43:24 -0600

    added Client stuff

--- Client/src/main/java/StartMenuController.java
+++ Client/src/main/java/StartMenuController.java
@@ -1,4 +1,4 @@
-import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -7,20 +7,20 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class StartMenuController {
+public class StartMenuController implements Initializable{
     @FXML Label reference;
+    Client clientConnection;
 
     public void handleSwitchToGamePlay() {
         //TODO need to handle the server stuff with this as well
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/GamePlayGUI.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/GamePlayGUI.fxml"));
             Scene gameScene = new Scene(loader.load());
-            gameScene.getStylesheets().add(getClass().getResource("styles/style1.css").toExternalForm());
+            gameScene.getStylesheets().add("/styles/style1.css");
             Stage stage = (Stage)(reference.getScene().getWindow());
             stage.setScene(gameScene);
 
@@ -34,6 +34,17 @@ public class StartMenuController {
     //Exits the game from the start button
     public void handleExitGame() {
         System.exit(0);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        clientConnection = new Client(data->{
+            Platform.runLater(()->{
+                System.out.println("Client Received: "+data.toString());
+            });
+        });
+
+        clientConnection.start();
     }
 
 }

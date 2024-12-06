808708cc065c683f8ae6ddc7b099926e5baf7674
Colin Grasley <147641960+ColinGrasley@users.noreply.github.com>
2024-12-06 14:57:28 -0600

    commented some stuff to start doing

--- Client/src/main/java/StartMenuController.java
+++ Client/src/main/java/StartMenuController.java
@@ -24,13 +24,28 @@ public class StartMenuController implements Initializable {
     public void handleSwitchToGamePlay() {
         //TODO need to handle the server stuff with this as well
         try {
+
+            //get the ip address and port number
+            //connect player client to server
+            
+
+           
+
+            //check if player is connected to the server
+            
+            //need try and catch for loading the scene for the client.
+            //loading the gameplay scene for the client after they have connected to the server.
             FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/GamePlayGUI.fxml"));
             Scene gameScene = new Scene(loader.load());
             gameScene.getStylesheets().add(getClass().getResource("styles/style1.css").toExternalForm());
             Stage stage = (Stage)(reference.getScene().getWindow());
             stage.setScene(gameScene);
 
-        } catch (IOException e) {
+            
+
+            
+
+        } catch (IOException e) { //catch for server connection
             e.printStackTrace();
             System.console().printf("Error: %s%n", e.getMessage());
         }

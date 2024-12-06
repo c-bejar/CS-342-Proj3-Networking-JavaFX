77dffce2dbc95f66f7781b3a81bbf6441bf46837
Colin Grasley <147641960+ColinGrasley@users.noreply.github.com>
2024-12-06 13:11:38 -0600

    Updated card class to include url of the card images

--- java/Card.java
+++ java/Card.java
@@ -1,11 +1,13 @@
 public class Card {
     private int value;
     private char suit;
+    private String imageURL;
 
     //constructor that doubles as a setter
     public Card(char suit, int value) {
         this.suit = suit;
         this.value = value;
+        this.imageULR = "/images/" + value + "of" + suit;
     }
 
     //getters for accessing object's value
@@ -15,8 +17,13 @@ public class Card {
     public int getValue() {
         return value;
     }
+    public String getImage() {
+        return String;
+    }
 
     public String output() {
         return suit + "" + value;
     }
+
+    public 
 }

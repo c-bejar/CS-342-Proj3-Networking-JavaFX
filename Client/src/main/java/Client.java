7030be04337db9faa4abf2d8b5e89d04e0fdb1cc
c-bejar <p.bejar.christopher@gmail.com>
2024-12-07 10:59:40 -0600

    added functionality for dealing hands

--- Client/src/main/java/Client.java
+++ Client/src/main/java/Client.java
@@ -2,10 +2,14 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.function.Consumer;
 
 public class Client extends Thread{
 
+    ArrayList<String> playersHand;
+    ArrayList<String> dealersHand;
+    boolean dealtHand = false;
     public Socket socketClient;
     public int port;
     private Consumer<Serializable> callback;
@@ -29,6 +33,42 @@ public class Client extends Thread{
             //e.printStackTrace();
             System.console().printf("Error: %s%n", e.getMessage());
         }
+    }
+
+    private void receiveHands(String data) {
+        ArrayList<String> dealerHand = new ArrayList<>();
+        ArrayList<String> playerHand = new ArrayList<>();
+
+        String[] parts = data.split("#");
+        for(int i = 0; i < parts.length; i++) {
+            String card1 = parts[i].substring(0, 2);
+            String card2 = parts[i].substring(2, 4);
+            String card3 = parts[i].substring(4, 6);
+            if(i == 0) {
+                dealerHand.add(card1);
+                dealerHand.add(card2);
+                dealerHand.add(card3);
+            } else {
+                playerHand.add(card1);
+                playerHand.add(card2);
+                playerHand.add(card3);
+            }
+        }
+        dealersHand = dealerHand;
+        playersHand = playerHand;
+        dealtHand = true;
+        System.out.println("Finished creating arrays for hands");
+    }
+
+    // Call different functions depending on what command is sent to Client
+    public void receiveString(Object in) {
+        try {
+            String data = in.toString();
+            System.out.println("data size: "+data.length());
+            if(data.length() == 13) {
+                receiveHands(data);
+            }
+        } catch(Exception e) {}
     }
 
     public void handleInput(PokerInfo data) {
@@ -59,6 +99,9 @@ public class Client extends Thread{
                         data = (PokerInfo) some;
                         handleInput(data);
                         System.out.println("client: sent data:" + data.command);
+                    } else if(some instanceof String) {
+                        receiveString(some);
+                        System.out.println("String sent!");
                     }
                 }
         } catch (Exception e) {

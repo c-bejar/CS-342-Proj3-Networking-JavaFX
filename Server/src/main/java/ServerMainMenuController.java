76a7051a1d317ac9bdfdc6bfb7387b943489b4a4
c-bejar <p.bejar.christopher@gmail.com>
2024-12-06 23:21:00 -0600

    toggle server

--- Server/src/main/java/ServerMainMenuController.java
+++ Server/src/main/java/ServerMainMenuController.java
@@ -1,20 +1,29 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.application.Platform;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Button;
 
 //second scene in the client UI. tracks the client data
 public class ServerMainMenuController implements Initializable {
 
     @FXML private VBox individualClientDataVBox;
+    @FXML private Button onOffServer;
+    @FXML private HBox sceneReference;
     @FXML private ListView clientList;
     @FXML private ListView clientJoinLog;
     @FXML private Label clientsLabel;
-    public Server serverConnection;
+    public static Server serverConnection;
     public static int portNum = ServerStartMenuController.portNumber;
 
     public void initialize(URL location, ResourceBundle resources) {
@@ -48,10 +57,32 @@ public class ServerMainMenuController implements Initializable {
     }
     @FXML
     public void handleDeactivateServerButton() {
-        //TODO: implement
+        serverConnection.acceptingClients = !serverConnection.acceptingClients;
+        if(onOffServer.getText().equals("Deactivate Server")) {
+            onOffServer.setText("Activate Server");
+        } else {
+            onOffServer.setText("Deactivate Server");
+        }
+
     }
     @FXML
     public void handleShutdownServerAndMenu() {
+        // try {
+        //     for(int i = 0; i < serverConnection.clients.size(); i++) {
+        //         serverConnection.clients.get(i).out.writeObject("Test");
+        //     }
+        // } catch(Exception e) {
 
+        // }
+        serverConnection.shutdown();
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXML/start.fxml"));
+            Scene startScene = new Scene(loader.load());
+            Stage stage = (Stage)(sceneReference.getScene().getWindow());
+            stage.setScene(startScene);
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.console().printf("Error: %s%n", e.getMessage());
+        }
     }
 }

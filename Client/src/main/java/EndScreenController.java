93c789c20b3fad5380ce1121c09e5c0252ff600b
c-bejar <p.bejar.christopher@gmail.com>
2024-12-07 21:24:25 -0600

    added logic for end screen display

--- Client/src/main/java/EndScreenController.java
+++ Client/src/main/java/EndScreenController.java
@@ -3,14 +3,20 @@ import java.util.ArrayList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class EndScreenController {
     @FXML VBox root;
+    @FXML Label gameStatus;
+    @FXML Button nextButton;
+    @FXML Button exitButton;
 
     Client clientSocket;
     int gameNum;
+    boolean atLeastQueenHigh;
     ArrayList<ArrayList<String>> logs;
 
     public void handleRetry() {
@@ -23,10 +29,12 @@ public class EndScreenController {
 
             GamePlayGUIController controller = loader.getController();
             controller.setClient(clientSocket);
+            controller.setBool(atLeastQueenHigh);
             controller.setAnteLabel();
             controller.updateWinningsLabel();
             controller.setGameNum(++gameNum);
             controller.setArray(logs);
+            controller.setLogs();
             stage.setScene(gameScene);
         } catch(Exception e) {
             e.printStackTrace();
@@ -37,9 +45,35 @@ public class EndScreenController {
         System.exit(0);
     }
 
+    public void setBool(boolean h) {
+        atLeastQueenHigh = h;
+    }
+
     // used outside of this file
-    public void setClient(Client clientSocket) {
+    public void setClient(Client clientSocket, char type) {
         this.clientSocket = clientSocket;
+        if(type == 'W') {
+            root.getStyleClass().remove("basic_background");
+            root.getStyleClass().add("win_background");
+            nextButton.getStyleClass().add("win_button");
+            exitButton.getStyleClass().add("win_button");
+            gameStatus.getStyleClass().add("win_label");
+            gameStatus.setText("You Win!");
+        }
+        else if(type == 'L') {
+            root.getStyleClass().add("lose_background");
+            nextButton.getStyleClass().add("lose_button");
+            exitButton.getStyleClass().add("lose_button");
+            gameStatus.getStyleClass().add("lose_label");
+            gameStatus.setText("You Lose!");
+        }
+        else{
+            gameStatus.setText("You Draw!");
+            root.getStyleClass().add("basic_background");
+            nextButton.getStyleClass().add("basic_button");
+            exitButton.getStyleClass().add("basic_button");
+            gameStatus.getStyleClass().add("basic_label");
+        }
     }
 
     // used outside of this file
